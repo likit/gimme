ccd941c1e2e72cf5d19b289251ca97714c500042
Likit Preeyanon <preeyano@msu.edu>
2012-01-16 23:35:02 -0500

    change a sequence name format from dir+number to prefix+number

--- utils/rename_fasta.py
+++ utils/rename_fasta.py
@@ -1,7 +1,7 @@
 #!/opt/local/bin/python
 ''' This script rename sequence names in fasta format.
 The new name will be in a format of
->directory name + a transcript number.
+>prefix + a transcript number.
 For example, >assembly1-39.
 Output is written to stdout.
 
@@ -13,12 +13,18 @@ Email   preeyano@msu.edu
 import sys, os
 
 input_file = sys.argv[1]
+try:
+    prefix = sys.argv[2]
+except:
+    prefix = ''
+else:
+    prefix = prefix+'-'
+
 n = 0
-dirname = os.getcwd().split(os.path.sep)[-1]
 
 for line in open(input_file):
     if line.startswith('>'):
-        print >> sys.stdout, '>%s-%d' % (dirname, n)
+        print >> sys.stdout, '>%s%d' % (prefix, n)
         n += 1
     else:
         print >> sys.stdout, line.strip(os.linesep)
