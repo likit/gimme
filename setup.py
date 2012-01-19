eb766c096aaf83ee1b98170de59083966cb25668
Likit Preeyanon <preeyano@msu.edu>
2012-01-18 19:45:06 -0500

    add setup.py for setuptools

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+        name = 'Gimme',
+        version = '0.7.4',
+        author = 'Likit Preeyanon',
+        author_email = 'preeyano@msu.edu',
+        license = 'GPL',
+
+        packages=find_packages('src'),
+        package_dir={'':'scr'},
+        )
