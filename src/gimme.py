49284a4858327035aaafa66704e3e38c9f0ea07f
Likit Preeyanon <preeyano@msu.edu>
2012-01-19 22:10:27 -0500

    add addPreviousEdges() and use it in collapseExons() also change a type of edges from list to set

--- src/gimme.py
+++ src/gimme.py
@@ -134,13 +134,14 @@ def collapseExons(g, exonDb):
                 if nextExon.terminal == 1:
                     g.add_edges_from([(str(currExon), n)\
                             for n in g.neighbors(str(nextExon))])
-
+                    addPreviousEdges(str(nextExon), str(currExon), g)
                     g.remove_node(str(nextExon))
                 else:
                     if currExon.terminal == 1:
                         if nextExon.start - currExon.start <= MIN_UTR:
                             g.add_edges_from([(str(nextExon), n)\
                                     for n in g.neighbors(str(currExon))])
+                            addPreviousEdges(str(currExon), str(nextExon), g)
                             g.remove_node(str(currExon))
 
                     currExon = nextExon
@@ -162,6 +163,7 @@ def collapseExons(g, exonDb):
                 if currExon.terminal == 2:
                     g.add_edges_from([(str(nextExon), n)\
                             for n in g.neighbors(str(currExon))])
+                    addPreviousEdges(str(currExon), str(nextExon), g)
 
                     g.remove_node(str(currExon))
                     currExon = nextExon
@@ -170,6 +172,7 @@ def collapseExons(g, exonDb):
                         if nextExon.end - currExon.end <= MIN_UTR:
                             g.add_edges_from([(str(currExon), n)\
                                     for n in g.neighbors(str(nextExon))])
+                            addPreviousEdges(str(nextExon), str(currExon), g)
 
                             g.remove_node(str(nextExon))
                         else:
@@ -237,12 +240,22 @@ def mergeClusters(exonDb):
     return bigCluster
 
 
+def addPreviousEdges(currExon, newExon, graph):
+    '''Add all upper edges of a current node to a new node.'''
+    edges = set([])
+    for upper_node in graph.predecessors(currExon):
+        edges.add((upper_node, newExon))
+        walkUpExonGraph(graph, upper_node, edges)
+    graph.add_edges_from(edges)
+
+
 def walkUpExonGraph(graph, exon, edges):
+    '''Return all upper edges of a given node.'''
     if graph.predecessors(exon) == []:
-        return
+        return edges
 
     for upper_node in graph.predecessors(exon):
-        edges.append((upper_node, exon))
+        edges.add((upper_node, exon))
         walkUpExonGraph(graph, upper_node, edges)
 
 
