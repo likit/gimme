abc350f48237fd49c1ff74bdcbe33c0d60193288
Likit Preeyanon <preeyano@msu.edu>
2012-01-24 23:04:13 -0500

    fix mispelling of src package. change a version number from 0.7.4 to 0.8.

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
         name = 'Gimme',
-        version = '0.7.4',
+        version = '0.8',
         author = 'Likit Preeyanon',
         author_email = 'preeyano@msu.edu',
         license = 'GPL',
 
         packages=find_packages('src'),
-        package_dir={'':'scr'},
+        package_dir={'':'src'},
 
         install_requires = [
                             'networkx',
