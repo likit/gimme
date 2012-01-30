1977ed103482afdb45a75f774771a312f9227b88
Likit Preeyanon <preeyano@msu.edu>
2012-01-30 10:58:23 -0500

    add GNU GPL license notice

--- src/gimme.py
+++ src/gimme.py
@@ -1,3 +1,24 @@
+'''
+    Gimme: a transcripts assembler based on alignments.
+
+    Copyright (C) 2012 Michigan State University
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Contact: Likit Preeyanon, preeyano@msu.edu
+'''
+
 import sys, csv
 import argparse
 from sys import stderr, stdout
