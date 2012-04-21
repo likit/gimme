1d51267f33e5b62a8934858b1f238d01ade723f7
Likit Preeyanon <preeyano@msu.edu>
2012-04-21 18:31:25 -0400

    kmers is now stored in a graph data structure
    graph algorithms are used to search for a bubble and traverse a path

--- src/utils/bubble.py
+++ src/utils/bubble.py
@@ -9,6 +9,9 @@ Email: preeyano@msu.edu
 import sys
 from collections import namedtuple
 
+import networkx as nx
+import matplotlib.pyplot as plt
+
 KMERSIZE = 27
 
 Sequence = namedtuple('Sequence', ['id', 'seq'])
@@ -31,57 +34,86 @@ def parse_fasta(filename):
     yield Sequence(seqid, seq)
 
 
-def makehash(sequence, kmersize=KMERSIZE):
-    kmer_table = {}
-    all_kmers = []
+def construct_kmer(sequence, kmersize=KMERSIZE):
     if sequence.seq < kmersize:
         return None
-    
-    kmerid = 0
+
+    kmer_table = {}
+    kmers = []
+    path = []
     end = kmersize
-    kmer = sequence.seq[:end]
-    kmer_table[kmer] = [kmerid]
-    all_kmers.append(kmer)
-    start = 1
-    end += 1
+    start = 0
+    kmer_id = 0
+
     while len(sequence.seq) - start >= kmersize:
-        kmerid += 1
         kmer = sequence.seq[start:end]
-        try:
-            kmer_table[kmer].append(kmerid)
-        except KeyError:
-            kmer_table[kmer] = [kmerid]
+        if kmer not in kmer_table:
+            kmer_table[kmer] = kmer_id
+            kmers.append(kmer)
+            kmer_id += 1
 
-        all_kmers.append(kmer)
-
-        #print >> sys.stderr, kmer, kmer_table[kmer]
+        path.append(kmer_table[kmer])
 
         start += 1
         end += 1
 
-    the_rest = sequence.seq[start:]
+    kmer_graph = nx.DiGraph()
+    kmer_graph.add_path(path)
+    print >> sys.stderr, 'total %d kmers' % len(kmers)
 
-    return kmer_table, all_kmers, the_rest
-
-
-def collapse(kmer_table, all_kmers):
-    discard = set()
-    for kmer in all_kmers:
-        if len(kmer_table[kmer]) > 0:
-            for node in kmer_table[kmer][1:]:
-                discard.add(node - 1)
-                discard.add(node)
-
-    return discard
+    return kmer_graph, kmers
 
 
-def rebuild_sequence(kmer_table, all_kmers, discard):
-    sequence = all_kmers[0]
-    for kmer in all_kmers[1:]:
-        if not set(kmer_table[kmer]).intersection(discard):
-            sequence += kmer[-1]
+def add_node(node_id, kmer_graph, path):
+    succ = kmer_graph.successors(node_id)
+    print node_id, succ
+    if succ == []:
+        return
+    else:
+        if len(succ) > 1:
+            for s in succ:
+                if s not in path:
+                    path.append(s)
+                    add_node(s, kmer_graph, path)
         else:
+            for s in succ:
+                path.append(s)
+                add_node(s, kmer_graph, path)
+
+
+
+def traverse(kmer_graph):
+    '''Return a path from all nodes.'''
+    root = kmer_graph.nodes()[0]
+    path = [root]
+    add_node(root, kmer_graph, path)
+
+    return path
+
+
+def collapse(kmer_graph, kmers):
+    for bubble in nx.algorithms.cycles.simple_cycles(kmer_graph):
+        mismatch = 0
+        try:
+            k2, k1 = bubble[-2], bubble[-1]
+        except IndexError:
             continue
+        else:
+            for i in range(len(kmers[k1-1])):
+                if kmers[k1-1][i] != kmers[k2][i]:
+                    mismatch += 1
+
+            if mismatch == 1:
+                try:
+                    kmer_graph.remove_node(k2)
+                except nx.NetworkXError:
+                    pass
+
+
+def rebuild_sequence(path, kmers):
+    sequence = kmers[0]
+    for k in path[1:]:
+        sequence += kmers[k][-1]
 
     return sequence
 
@@ -93,21 +125,29 @@ def main(argv):
     except IndexError:
         kmersize = None
 
-    log_file = open(fasta_file+'.log', 'w')
+    #log_file = open(fasta_file+'.log', 'w')
 
     for n, sequence in enumerate(parse_fasta(fasta_file), start=1):
-        kmer_table, all_kmers, the_rest = makehash(sequence, kmersize)
-        discard = collapse(kmer_table, all_kmers)
-        new_sequence = rebuild_sequence(kmer_table, all_kmers, discard)
-        print '>%s\n%s' % (sequence.id, new_sequence)
-        if discard:
-            print >> log_file, '%s\t%d\t%d'\
-                    % (sequence.id, len(sequence.seq), len(discard))
+        print >> sys.stderr, sequence.id,
+        kmer_graph, kmers = construct_kmer(sequence, kmersize)
+        if nx.algorithms.cycles.simple_cycles(kmer_graph):
+            collapse(kmer_graph, kmers)
+            path = traverse(kmer_graph)
+            new_sequence = rebuild_sequence(path, kmers)
+            print '>%s\n%s' % (sequence.id, new_sequence)
+        else:
+            new_sequence = sequence.seq
+
+        print >> sys.stderr, '%s\t%d\t%d'\
+            % (sequence.id, len(sequence.seq) - len(new_sequence), len(sequence.seq))
 
         if n % 1000 == 0:
             print >> sys.stderr, '...', n
 
-    log_file.close()
+        #nx.draw(kmer_graph)
+        #plt.show()
+
+    #log_file.close()
 
 
 if __name__=='__main__':
