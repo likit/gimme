d7e8017fe1dd06fac4341c336201c04e57a2f988
Likit Preeyanon <preeyano@msu.edu>
2012-05-09 11:05:10 -0400

    added assignCDS.py

---
+++ src/utils/assignCDS.py
@@ -0,0 +1,123 @@
+import sys, csv
+from bx.intervals.intersection import Interval, Intersecter
+
+
+class ExonObj(object):
+    def __init__(self, seqId, start, end, cStart, cEnd, size):
+        self.seqId = seqId
+        self.start = start
+        self.end = end
+        self.cStart = cStart
+        self.cEnd = cEnd
+        self.geneSize = size
+
+    def __str__(self):
+        return '%s:%d-%d' % (self.seqId, self.start, self.end)
+
+
+def parseBed(filename):
+    '''Reads BED file and returns a group of exons
+    of a transcript.
+
+    '''
+
+    with open(filename) as fp:
+        for row in csv.reader(fp, dialect='excel-tab'):
+            exons = []
+            seqId = row[3]
+            chromStart = int(row[1])
+
+            blockSizes = [int(i) for i in row[-2].split(',')]
+            chromStarts = [int(i) + chromStart
+                                for i in row[-1].split(',')]
+            chromEnds = [int(blockSizes[i]) + chromStarts[i]\
+                                for i in range(len(blockSizes))]
+            size = sum(blockSizes)
+
+            if len(blockSizes) == 1:
+                continue
+            
+            i = 0
+            start = i
+            while i in range(len(blockSizes)):
+                end = start + blockSizes[i]
+                exons.append(ExonObj(seqId,
+                                        start,
+                                        end,
+                                        chromStarts[i],
+                                        chromEnds[i],
+                                        size),
+                                        )
+                i += 1
+                start = end
+
+            yield row, exons
+
+
+def parseCDS(filename):
+    cds = {}
+    for line in open(filename):
+        if line.startswith('>'):
+            line = line.strip('>')
+            strand = '-' if 'minus' in line else '+'
+            values = line.split()
+            name = values[0].strip('>').strip(';')
+            length = int(values[4].split('=')[-1].strip(';'))
+            if 'blastx' in line:
+                if strand == '+':
+                    start = int(values[2]) - 1
+                    end = int(values[3]) - 1
+                else:
+                    start = length - int(values[3])
+                    end = length - int(values[2])
+            else:
+                start = int(values[2]) - 1
+                end = int(values[3]) - 1
+
+            assert start < end
+            cds[name] = (name, start, end, strand)
+
+    return cds
+
+
+def buildIntervalTree(exons):
+    '''Build interval tree from exon annotations.'''
+    tree = Intersecter()
+    for exon in exons:
+        tree.add_interval(Interval(exon.start, exon.end,
+                            value={'cStart':exon.cStart,
+                                    'cEnd':exon.cEnd}))
+    return tree
+
+def findCDS(bedFile, cds):
+    """docstring for findCDS"""
+
+    writer = csv.writer(sys.stdout, dialect='excel-tab')
+    for row, exons in parseBed(bedFile):
+        tree = buildIntervalTree(exons)
+
+        exon = exons[0]
+        try:
+            name, start, end, strand = cds[exon.seqId]
+        except KeyError:
+            row[5] = '.'
+            writer.writerow(row)
+        else:
+            try:
+                exon1 = tree.find(start, start + 1)[0]
+                exon2 = tree.find(end - 1, end)[0]
+            except IndexError:
+                print >> sys.stderr, name, start, end, strand
+            else:
+                CDSStart = exon1.value['cStart'] + (start - exon1.start)
+                CDSEnd = exon2.value['cStart'] + (end - exon2.start)
+                row[5] = strand
+                row[6] = CDSStart
+                row[7] = CDSEnd
+                writer.writerow(row)
+
+
+if __name__=='__main__':
+    print >> sys.stderr, 'Parsing FASTA file...'
+    cds = parseCDS(sys.argv[1])
+    findCDS(sys.argv[2], cds)
