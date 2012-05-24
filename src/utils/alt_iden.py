e28cdd70376cebc681b9493454645ac70859c6b2
Likit Preeyanon <preeyano@msu.edu>
2012-05-24 17:58:35 -0400

    added alt_iden.py with skipped_exons()

---
+++ src/utils/alt_iden.py
@@ -0,0 +1,23 @@
+'''This script identifies several kinds of alternative splicings
+from given gene models.
+
+The script now only supports gene models in BED format.
+
+'''
+
+def skipped_exon(gene):
+    '''Identify skipped exons, if exists in a gene.
+    gene is a DiGraph object containing exons and introns as
+    nodes and edges respectively.
+
+    '''
+
+    skipped_exons = set([])
+    for node in gene.nodes():
+        neighbors = gene.neighbors(node)
+        for nb in neighbors:
+            for s in gene.successors(nb):
+                if s in neighbors:
+                    skipped_exons.add((node.name,s.name))
+
+    return skipped_exons
