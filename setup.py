406bf4ae25f68567011830c2e8b99b8d28c28a06
Likit Preeyanon <preeyano@msu.edu>
2012-06-09 09:46:26 -0400

    added a setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+        # basic package data
+        name = "Gimme tools",
+        version = "0.9",
+
+        # package structure
+        packages=find_packages('src'),
+        package_dir={'src'},
+        )
