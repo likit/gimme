15e09d129633ce81c0eeede79c644f66a38e15e4
Likit Preeyanon <preeyano@msu.edu>
2012-09-05 16:36:13 -0400

    edited the script to read gene models from BED file directly.

--- src/utils/find_match.py
+++ src/utils/find_match.py
@@ -2,12 +2,11 @@
 
 transcripts/genes matching sequences in a database.
 
-(run blat with -out=blast9 to get an output in Blast9
-format)
+(run blat with -out=blast9 to get an output in Blast9 format)
 
 input :
-    1) transcript lengths (from get_transcript_length.py)
-    2) alignments in Blast9 format
+    1) Gene models in BED format
+    2) Alignments in Blast9 format
 
 '''
 
@@ -15,7 +14,7 @@ import sys
 
 ''' a query must cover at least a given
 percent of a sequence in a database.'''
-PERCENT_COVERAGE = 70
+PERCENT_COVERAGE = 80
 
 
 def find_match(filename, trans_len):
@@ -39,11 +38,19 @@ def find_match(filename, trans_len):
 def get_transcript_length(filename):
     trans_len = {}
     for line in open(filename):
-        gene, trans, size = line.strip().split('\t')
-        trans_len[trans] = int(size)
+        cols = line.split('\t')
+        gene_id, trans_id = cols[3].strip().split('.')
+        size = sum([int(block) for block in cols[10].split(',')])
+        trans_len[trans_id] = size
 
     return trans_len
 
 if __name__=='__main__':
-    trans_len = get_transcript_length(sys.argv[1])
-    genes, transcripts = find_match(sys.argv[2], trans_len)
+    try:
+        trans_len = get_transcript_length(sys.argv[1])
+        genes, transcripts = find_match(sys.argv[2], trans_len)
+    except IndexError:
+        print >> sys.stderr, "find_match.py <BED file> <Blast9 file>"
+    else:
+        for gene in genes:
+            print gene
