8712153470f7714a7addfa779075ed83597902c1
Likit Preeyanon <preeyano@msu.edu>
2012-09-22 18:07:01 -0400

    changed a version number
    changed a networkx version to 1.7

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages
 setup(
         # basic package data
         name = "Gimme tools",
-        version = "0.9",
+        version = "0.97",
 
         # package structure
         packages=find_packages('src'),
         package_dir={'':'src'},
 
         install_requires = [
-                            'networkx == 1.5',
+                            'networkx == 1.7',
                             'pygr == 0.8.2',
                             'bx-python == 0.7.1',
                             ]
