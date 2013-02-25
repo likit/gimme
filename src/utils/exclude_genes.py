f05ad533466d0a1bc1647cdb6752a6fb0a01ce1a
Likit Preeyanon <preeyano@msu.edu>
2013-02-25 01:16:08 -0500

    cleaned up PEP8 violations

--- src/utils/exclude_genes.py
+++ src/utils/exclude_genes.py
@@ -3,11 +3,13 @@ import csv
 
 MAX_ISOFORM = 50
 
+
 def parse_BED(filename):
     reader = csv.reader(open(filename), dialect='excel-tab')
     for row in reader:
         transcript_id = row[3].replace(':', '-')
         yield transcript_id, row
+
 
 def main():
     infile = sys.argv[1]
@@ -17,13 +19,14 @@ def main():
     for transcript_id, row in parse_BED(infile):
         new_id = transcript_id.split('.')[0]
         print >> sys.stderr, current_id, new_id
-        if not current_id: # first gene
+        if not current_id:  # first gene
             current_id = new_id
             transcripts.append(row)
         else:
             if new_id != current_id:
                 if len(transcripts) <= MAX_ISOFORM:
-                    for trn in transcripts: writer.writerow(trn)
+                    for trn in transcripts:
+                        writer.writerow(trn)
 
                 current_id = new_id
                 transcripts = []
@@ -31,7 +34,8 @@ def main():
             transcripts.append(row)
 
     if len(transcripts) <= MAX_ISOFORM:
-        for trn in transcripts: writer.writerow(trn)
+        for trn in transcripts:
+            writer.writerow(trn)
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
