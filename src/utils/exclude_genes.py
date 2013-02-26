2c2f890438ab8bdcee7756fc6fcf2f74dad304c8
Likit Preeyanon <preeyano@msu.edu>
2013-02-26 03:40:53 -0500

    replaced a hardcoded max isoform with  a user-defined one as well as  a default value.
    added a report of number of excluded genes

--- src/utils/exclude_genes.py
+++ src/utils/exclude_genes.py
@@ -1,8 +1,6 @@
 import sys
 import csv
 
-MAX_ISOFORM = 50
-
 
 def parse_BED(filename):
     reader = csv.reader(open(filename), dialect='excel-tab')
@@ -13,29 +11,43 @@ def parse_BED(filename):
 
 def main():
     infile = sys.argv[1]
+    try:
+        max_isoform = int(sys.argv[2])
+    except IndexError:
+        max_isoform = 50
+        print >> sys.stderr, 'Default max isoform = 50'
+    else:
+        print >> sys.stderr, 'Max isoform = %d' % max_isoform
+
+    excluded = 0
     current_id = None
     writer = csv.writer(sys.stdout, dialect='excel-tab')
     transcripts = []
     for transcript_id, row in parse_BED(infile):
         new_id = transcript_id.split('.')[0]
-        print >> sys.stderr, current_id, new_id
         if not current_id:  # first gene
             current_id = new_id
             transcripts.append(row)
         else:
             if new_id != current_id:
-                if len(transcripts) <= MAX_ISOFORM:
+                if len(transcripts) <= max_isoform:
                     for trn in transcripts:
                         writer.writerow(trn)
+                else:
+                    excluded += 1
 
                 current_id = new_id
                 transcripts = []
 
             transcripts.append(row)
 
-    if len(transcripts) <= MAX_ISOFORM:
+    if len(transcripts) <= max_isoform:
         for trn in transcripts:
             writer.writerow(trn)
+    else:
+        excluded += 1
+
+    print >> sys.stderr, 'Total %d genes excluded.' % excluded
 
 if __name__ == '__main__':
     main()
