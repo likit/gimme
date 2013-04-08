20554b0bb131f9dd3597ded58bd3b32961aa5977
Likit Preeyanon <preeyano@msu.edu>
2013-04-07 22:37:32 -0400

    added more tests for negative strand

--- tests/find_AFE_test.py
+++ tests/find_AFE_test.py
@@ -20,7 +20,7 @@ from utils.find_AFE import get_exon_node, Exon, find_AFE
 #             self.assertEqual(len(exons), len(self.graph.nodes()))
 
 
-class TestFindAFE(unittest.TestCase):
+class TestFindAFEPositive(unittest.TestCase):
     def setUp(self):
         self.exonsDB = {}
         self.graph = nx.DiGraph()
@@ -115,6 +115,98 @@ class TestFindAFE(unittest.TestCase):
         path1 = [str(self.ex1), str(self.ex2), str(self.ex3)]
         path2 = [str(self.ex5), str(self.ex8), str(self.ex3)]
         path3 = [str(self.ex6), str(self.ex4), str(self.ex3)]
+        self.transcripts = [path1, path2, path3]
+
+        self.graph.add_path(path1)
+        self.graph.add_path(path2)
+        self.graph.add_path(path3)
+
+        paths = find_AFE(self.graph, self.exonsDB, self.transcripts)
+        num_paths = [len(path) for path in paths]
+
+        self.assertEqual(len(paths), 3)
+        self.assertItemsEqual(num_paths, [3, 3, 3])
+
+
+class TestFindAFENegative(unittest.TestCase):
+    def setUp(self):
+        self.exonsDB = {}
+        self.graph = nx.DiGraph()
+        self.ex1 = Exon('chrX', 1000, 2000, 'ex1.1', '-')
+        self.ex2 = Exon('chrX', 3000, 4000, 'ex1.1', '-')
+        self.ex3 = Exon('chrX', 5000, 6000, 'ex1.1', '-')
+        self.ex4 = Exon('chrX', 800, 900, 'ex1.1', '-')
+        self.ex5 = Exon('chrX', 1500, 2500, 'ex1.1', '-')
+        self.exonsDB[str(self.ex1)] = self.ex1
+        self.exonsDB[str(self.ex2)] = self.ex2
+        self.exonsDB[str(self.ex3)] = self.ex3
+        self.exonsDB[str(self.ex4)] = self.ex4
+        self.exonsDB[str(self.ex5)] = self.ex5
+
+    def test_negative_one_path_one_exon(self):
+        self.graph.add_path([str(self.ex1), str(self.ex2)])
+        self.transcripts = [[str(self.ex1), str(self.ex2)]]
+        paths = find_AFE(self.graph, self.exonsDB, self.transcripts)
+
+        self.assertEqual(len(paths), 0)
+
+    def test_negative_two_path_one_exon(self):
+        path1 = [str(self.ex1), str(self.ex2)]
+        path2 = [str(self.ex1), str(self.ex3)]
+        self.graph.add_path(path1)
+        self.graph.add_path(path2)
+        self.transcripts = [path1, path2]
+        paths = find_AFE(self.graph, self.exonsDB, self.transcripts)
+        num_paths = [len(path) for path in paths]
+
+        self.assertEqual(len(paths), 2)
+        self.assertItemsEqual(num_paths, [2, 2])
+
+    def test_negative_two_path_multiple_exon(self):
+        self.ex6 = Exon('chrX', 7000, 8000, 'ex1.1', '-')
+        self.ex7 = Exon('chrX', 9000, 10000, 'ex1.1', '-')
+        self.exonsDB[str(self.ex6)] = self.ex6
+        self.exonsDB[str(self.ex7)] = self.ex7
+
+        path1 = [str(self.ex1), str(self.ex2),
+                    str(self.ex3), str(self.ex6)]
+        path2 = [str(self.ex1), str(self.ex7)]
+        self.graph.add_path(path1)
+        self.graph.add_path(path2)
+        self.transcripts = [path1, path2]
+
+        paths = find_AFE(self.graph, self.exonsDB, self.transcripts)
+        num_paths = [len(path) for path in paths]
+
+        self.assertEqual(len(paths), 2)
+        self.assertItemsEqual(num_paths, [4, 2])
+
+    def test_negative_three_path_one_exon(self):
+        path1 = [str(self.ex1), str(self.ex2)]
+        path2 = [str(self.ex1), str(self.ex3)]
+        path3 = [str(self.ex1), str(self.ex5)]
+        self.transcripts = [path1, path2, path3]
+        self.graph.add_path(path1)
+        self.graph.add_path(path2)
+        self.graph.add_path(path3)
+
+        paths = find_AFE(self.graph, self.exonsDB, self.transcripts)
+        num_paths = [len(path) for path in paths]
+
+        self.assertEqual(len(paths), 3)
+        self.assertItemsEqual(num_paths, [2, 2, 2])
+
+    def test_negative_three_path_two_exon(self):
+        self.ex4 = Exon('chrX', 7500, 8500, 'ex1.1', '+')
+        self.ex6 = Exon('chrX', 3500, 4500, 'ex1.1', '+')
+        self.ex8 = Exon('chrX', 5500, 6500, 'ex1.1', '+')
+        self.exonsDB[str(self.ex4)] = self.ex4
+        self.exonsDB[str(self.ex6)] = self.ex6
+        self.exonsDB[str(self.ex8)] = self.ex8
+
+        path1 = [str(self.ex1), str(self.ex2), str(self.ex3)]
+        path2 = [str(self.ex1), str(self.ex5), str(self.ex4)]
+        path3 = [str(self.ex1), str(self.ex6), str(self.ex8)]
         self.transcripts = [path1, path2, path3]
 
         self.graph.add_path(path1)
