5987afdcf0e16974bc9c886bd251dd9671517995
Likit Preeyanon <preeyano@msu.edu>
2013-05-15 23:43:23 -0400

    added two more tests to test merged exons
    fixed errors in test_no_ALE_with_AFE()

--- tests/find_ALE_test.py
+++ tests/find_ALE_test.py
@@ -55,7 +55,7 @@ class TestFindALEPositive(unittest.TestCase):
 
         path1 = ['start', str(self.ex1), str(self.ex2),
                    str(self.ex3), 'end']
-        path2 = ['start', str(self.ex1), str(self.ex5),
+        path2 = ['start', str(self.ex4), str(self.ex2),
                    str(self.ex3), 'end']
 
         self.graph.add_path(path1)
@@ -183,6 +183,58 @@ class TestFindALEPositive(unittest.TestCase):
         self.assertEqual(len(paths), 3)
         self.assertItemsEqual(num_exons, [3, 3, 3])
 
+    def test_positive_two_path_merged_exon(self):
+        '''
+            []---->[*********]
+            []---->[]------>[]
+        '''
+        self.ex8 = Exon('chrX', 2000, 5000, 'ex1.1', '+')
+        self.exonsDB[str(self.ex8)] = self.ex8
+
+        path1 = ['start', str(self.ex1),
+                    str(self.ex2), str(self.ex3), 'end']
+        path2 = ['start', str(self.ex1), str(self.ex8), 'end']
+
+        self.transcripts = [path1, path2]
+
+        self.graph.add_path(path1)
+        self.graph.add_path(path2)
+
+        paths = find_ALE(self.graph, self.exonsDB, self.transcripts)
+        num_exons = [len(path) for path in paths]
+
+        self.assertEqual(len(paths), 2)
+        self.assertItemsEqual(num_exons, [3, 2])
+
+    def test_positive_three_path_merged_exon(self):
+        '''
+            []-----[]---->[*********]
+            []---->[]--->[]
+            []-----[======]------>[]
+        '''
+        self.ex7 = Exon('chrX', 1000, 4000, 'ex1.1', '+')
+        self.exonsDB[str(self.ex7)] = self.ex7
+        self.ex8 = Exon('chrX', 2000, 5000, 'ex1.1', '+')
+        self.exonsDB[str(self.ex8)] = self.ex8
+
+        path1 = ['start', str(self.ex4), str(self.ex1),
+                    str(self.ex2), 'end']
+        path2 = ['start', str(self.ex4), str(self.ex1),
+                    str(self.ex8), 'end']
+        path3 = ['start', str(self.ex4), str(self.ex7),
+                    str(self.ex3), 'end']
+
+        self.transcripts = [path1, path2, path3]
+
+        self.graph.add_path(path1)
+        self.graph.add_path(path2)
+        self.graph.add_path(path3)
+
+        paths = find_ALE(self.graph, self.exonsDB, self.transcripts)
+        # num_exons = [len(path) for path in paths]
+
+        self.assertEqual(len(paths), 3)
+        # self.assertItemsEqual(num_exons, [3, 2])
 
 class TestFindALENegative(unittest.TestCase):
     def setUp(self):
